4c5575da90edfdc90d2292bf674f3ec8581de465
Pablo Fraile Alonso <pablo@pablofraile.net>
2025-08-25 12:41:42 +0200

    build: add nix and delete junk

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "AMSA 25-26 slides";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable?shallow=1";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";  # or "aarch64-darwin" etc. depending on your system
+      pkgs = import nixpkgs { inherit system; };
+      quarto = pkgs.quarto;
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [ quarto ];
+      };
+      packages.${system}.default = pkgs.stdenv.mkDerivation {
+        pname = "amsa-slides";
+        version = "1.0.0";
+        src = ./.;
+        buildInputs = [ quarto ];
+        buildPhase = ''
+          # Deno needs to add stuff to $HOME/.cache
+          # so we give it a home to do this
+          mkdir home
+          export HOME=$PWD/home
+          quarto render --no-cache
+        '';
+        installPhase = ''
+          mkdir -p $out
+          cp -r docs/* $out/
+        '';
+      };
+    };
+}
