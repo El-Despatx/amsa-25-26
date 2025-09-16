43868f855179737fe5a128a4822a6a6826436762
Pablo Fraile Alonso <pablo@pablofraile.net>
2025-09-16 21:43:57 +0200

    feat: add download button as pdf for activities

--- flake.nix
+++ flake.nix
@@ -25,13 +25,23 @@
       });
     in {
       devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [ quarto-dev pkgs.vscode pkgs.pandoc ];
+        buildInputs = [ quarto-dev pkgs.vscode pkgs.pandoc pkgs.fontconfig pkgs.texliveTeTeX ];
+        shellHook = ''
+          export LD_LIBRARY_PATH=${pkgs.lib.getLib pkgs.fontconfig}/lib:${pkgs.freetype}/lib:$LD_LIBRARY_PATH
+          # export LD_PRELOAD=${pkgs.fontconfig}/lib/libfontconfig.so.1:${pkgs.freetype}/lib/libfreetype.so.6
+        '';
       };
       packages.${system}.default = pkgs.stdenv.mkDerivation {
         pname = "amsa-slides";
         version = "2025.2026.v1";
         src = ./src;
-        buildInputs = [ quarto pkgs.which ];
+        buildInputs = [ quarto pkgs.which pkgs.pandoc pkgs.fontconfig (pkgs.texlive.combine {
+        inherit (pkgs.texlive)
+        scheme-medium  # or scheme-small if you want minimal
+        collection-latexextra
+        xcolor         # since your doc also uses \definecolor
+        ;
+    }) ];
         buildPhase = ''
           # Deno needs to add stuff to $HOME/.cache
           # so we give it a home to do this
